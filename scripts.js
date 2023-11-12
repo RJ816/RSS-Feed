b41de08b8e8127a5e15c1d7c9edc43e1a650d3d6
RJ816 <robin.jiang14@gmail.com>
2023-11-12 14:15:43 -0600

    added a xmlparser function and txt of rss feeds

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-document.getElementById("submit").addEventListener("click", storeUrl);
+document.getElementById("submit").addEventListener("click", processRss);
+
+function processRss() {
+    storeUrl();
+    parseRss();
+}
 
 function storeUrl() {
     let url = document.getElementById("url").value;
-    let storedUrls = localStorage.getItem("urlArray");
-    let urlArray = storedUrls ? JSON.parse(storedUrls) : [];
-    urlArray.push(url);
-    urlArray = Array.from(new Set(urlArray));
-    localStorage.setItem("urlArray", JSON.stringify(urlArray));
-    fetchRss(url);
+    if (validateUrl(url)) {
+        let storedUrls = localStorage.getItem("urlArray");
+        let urlArray = storedUrls ? JSON.parse(storedUrls) : [];
+        urlArray.push(url);
+        urlArray = Array.from(new Set(urlArray));
+        localStorage.setItem("_urlArray", JSON.stringify(urlArray));
+        fetchRss(url);
+    } else {
+        console.log("invalid url");
+    }
+}
+
+function fetchRss(url) {
+    const corsProxyUrl = "https://cors-anywhere.herokuapp.com/"; //TODO remove
+    let key = extractSecondLevelDomain(url);
+    fetch(corsProxyUrl + url)
+    .then(response => response.text())
+    .then(xmlData => {localStorage.setItem(key, xmlData);});
 }
 
 function extractSecondLevelDomain(url) {
@@ -18,9 +35,17 @@ function extractSecondLevelDomain(url) {
     return secondLevelDomain;
 } 
 
-function fetchRss(url) {
-    const corsProxyUrl = "https://cors-anywhere.herokuapp.com/"; //TODO remove
-    fetch(corsProxyUrl + url)
-    .then(response => response.text())
-    .then(xmlData => {localStorage.setItem("test", xmlData);});
+function validateUrl(url) {
+    if (url === "") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function parseRss() {
+    let text = localStorage.getItem("microsoft");
+    const xmlParser = new DOMParser();
+    let xmlRss = xmlParser.parseFromString(text,"text/xml");
+    document.getElementById("test").innerHTML = text;
 }
