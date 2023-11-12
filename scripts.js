f8e193464d3315a933c2caaf97b3a69fa5fddb90
RJ816 <robin.jiang14@gmail.com>
2023-11-12 01:34:22 -0600

    localStorage for url submit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-function addUrl(url) {
-    //TODO create Validation function
-    if (url != undefined) {
-        localStorage.setItem("test", url);
-    }
-    document.getElementById().innerHTML = localStorage.getItem("test");
+document.getElementById("submit").addEventListener("click", storeUrl);
+
+function storeUrl() {
+    let url = document.getElementById("url").value;
+    let key = extractSecondLevelDomain(url);
+    localStorage.setItem(key, url);
 }
 
+function extractSecondLevelDomain(url) {
+    const urlObj = new URL(url);
+    let hostname = urlObj.hostname;
+    const array = hostname.split('.');
+    let secondLevelDomain = array[array.length - 2];
+    return secondLevelDomain;
+} 
