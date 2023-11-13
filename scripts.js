793845bfa14302eac1f41b6f01776aa99c878a16
RJ816 <robin.jiang14@gmail.com>
2023-11-13 01:09:06 -0600

    save

--- scripts.js
+++ scripts.js
@@ -21,19 +21,19 @@ function storeUrl() {
 
 function fetchRss(url) {
     const corsProxyUrl = "https://cors-anywhere.herokuapp.com/"; //TODO remove
-    let key = extractSecondLevelDomain(url);
+    let key = url;
     fetch(corsProxyUrl + url)
     .then(response => response.text())
     .then(xmlData => {localStorage.setItem(key, xmlData);});
 }
 
-function extractSecondLevelDomain(url) {
-    const urlObj = new URL(url);
-    let hostname = urlObj.hostname;
-    const array = hostname.split('.');
-    let secondLevelDomain = array[array.length - 2];
-    return secondLevelDomain;
-} 
+// function extractSecondLevelDomain(url) {
+//     const urlObj = new URL(url);
+//     let hostname = urlObj.hostname;
+//     const array = hostname.split('.');
+//     let secondLevelDomain = array[array.length - 2];
+//     return secondLevelDomain;
+// } 
 
 function validateUrl(url) {
     if (url === "") {
@@ -44,8 +44,32 @@ function validateUrl(url) {
 }
 
 function parseRss() {
-    let text = localStorage.getItem("microsoft");
+    let text = localStorage.getItem("https://calendar.gatech.edu/event-calendar-day.xml");
     const xmlParser = new DOMParser();
     let xmlObj = xmlParser.parseFromString(text,"text/xml");
-    document.getElementById("test").innerHTML = text;
+    let title = xmlObj.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    let link = xmlObj.getElementsByTagName("link")[0].childNodes[0].nodeValue;
+    let description = xmlObj.getElementsByTagName("description")[0].childNodes[0].nodeValue;
+    const channelObj = new RssDom(title, link, description);
+    let feedContainer = document.getElementById("feedContainer");
+    let titleElement = document.createElement("div");
+    let linkElement = document.createElement("div");
+    let descriptionElement = document.createElement("div");
+    titleElement.innerHTML = channelObj.getChannelTitle();
+    linkElement.innerHTML = channelObj.getChannelLink();
+    descriptionElement.innerHTML = channelObj.getChannelDescription();
+    feedContainer.appendChild(titleElement);
+    feedContainer.appendChild(linkElement);
+    feedContainer.appendChild(descriptionElement);
+}
+
+class RssDom {
+    constructor (channelTitle, channelLink, channelDescription) {
+        this.channelTitle = channelTitle;
+        this.channelLink = channelLink;
+        this.channelDescription = channelDescription;
+    }
+    getChannelTitle() {return this.channelTitle};
+    getChannelLink() {return this.channelLink};
+    getChannelDescription() {return this.channelDescription};
 }
