676ba8de018f62976a0bd6e9b95b727b03890cb1
Robin Jiang <robin.jiang14@gmail.com>
2023-11-16 14:19:46 -0600

    add node.js server

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var http = require('http');
+let http = require("http");
+let fs = require("fs");
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  fs.readFile("index.html", function (err, data) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(data);
+    return res.end();
+  });  
+
 }).listen(8080);
