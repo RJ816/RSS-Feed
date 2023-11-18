c454d8b22a373fc3318cf8b6b1712b60195f3808
Robin Jiang <robin.jiang14@gmail.com>
2023-11-18 14:13:31 -0600

    added react index css

--- rss-app/src/header/navbar/add-url-link/Form.tsx
+++ rss-app/src/header/navbar/add-url-link/Form.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Form() {
     return (<form>
         <label htmlFor="url">Add RSS URL</label>
-        <input name="url" id="url" type="url" />
+        <input name="url" id="url" type="url" placeholder="Enter RSS URL" />
         <button id="submit" type="button">Submit</button>
     </form>);
 }
--- rss-app/src/index.tsx
+++ rss-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import Header from "./header/Header.tsx";
 import { App } from "./App.tsx";
 
