d0bd87907f4752e949bdd4b73104bd47a88e59b7
Robin Jiang <robin.jiang14@gmail.com>
2023-11-18 21:51:27 -0600

    save

--- rss-app/server/node-server.js
+++ rss-app/server/node-server.js
@@ -1,11 +1,6 @@
-let http = require("http");
-let fs = require("fs");
+var http = require("http");
 
 http.createServer(function (req, res) {
-  fs.readFile("index.html", function (err, data) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(data);
-    return res.end();
-  });  
-
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
 }).listen(8080);
