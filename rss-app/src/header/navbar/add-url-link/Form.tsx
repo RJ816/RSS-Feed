27533c23bec95155814caeaf16061c3fc0c66e4a
Robin Jiang <robin.jiang14@gmail.com>
2023-11-18 18:04:43 -0600

    save

--- rss-app/src/header/navbar/add-url-link/Form.tsx
+++ rss-app/src/header/navbar/add-url-link/Form.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+//import fs from "fs";
 
 function Form() {
-    return (<form>
+    const [url, setUrl] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (url.trim() === "") {
+            alert("Please enter a non-empty URL");
+          } else {
+            alert(`The url you entered was: ${url}`);
+            // Now you can proceed with any further actions, such as making a request to the server.
+          }
+      }
+
+    return (<form onSubmit={handleSubmit}>
         <label htmlFor="url">Add RSS URL</label>
-        <input name="url" id="url" type="url" placeholder="Enter RSS URL" />
-        <button id="submit" type="button">Submit</button>
+        <input 
+            name="url" id="url" 
+            type="url" 
+            placeholder="Enter RSS URL" 
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+        />
+        <input type="submit" /> 
     </form>);
 }
 
