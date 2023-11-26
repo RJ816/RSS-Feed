6c22e7ae8d2e3d54f6dc960cb1f709de8c614356
Your Name <your_email@example.com>
2023-11-26 00:25:19 -0600

    save

--- app/add-url/lib/parse-xml.ts
+++ app/add-url/lib/parse-xml.ts
@@ -1,15 +1,17 @@
 import Item from "./item-class";
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-export default function XmlParse(url: string, text: string) {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(text,"text/xml");
+export default function parseXml(url: string, text: string) {
+    const dom = new JSDOM(text, { contentType: "text/xml" });
+    const xmlDoc = dom.window.document;
     const items = xmlDoc.querySelectorAll("item"); 
-    const itemArray = Array.from(items).map((item) => {
+    const itemArray = Array.from(items).map((item: any) => {
         const title = item.querySelector("title")?.textContent || "";
         const link = item.querySelector("link")?.textContent || "";
         const pubDate = item.querySelector("pubDate")?.textContent || undefined;
         return new Item(title, link, pubDate);
-      });   
+      });
     // Structure the result as a JSON object with the url as the key
     const jsonObj: Record<string, Item[]> = { [url]: itemArray };
     
--- app/add-url/lib/send-url.ts
+++ app/add-url/lib/send-url.ts
@@ -2,13 +2,13 @@
 
 import fetchRss from "../api/fetch-rss";
 import storeJson from "./store-json";
-import XmlParse from "./xml-parse";
+import parseXml from "./parse-xml";
 
 export default async function SendUrl(formData: FormData) {
     let url = formData.get("url") as string;
     const rss = await fetchRss(url);
     const urlPath = "app/database/rss-urls.json";
-    const jsonObj = XmlParse(url, rss);
+    const jsonObj = parseXml(url, rss);
     
     storeJson(urlPath, url, url);
     
