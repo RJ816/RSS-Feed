3ba3daae83d91282feb0fcdc3c55b1433ffec348
Your Name <your_email@example.com>
2023-11-29 02:34:34 -0600

    save

--- app/ui/main/main.tsx
+++ app/ui/main/main.tsx
@@ -1,20 +1,33 @@
 "use client";
-
-import { createContext, useState } from "react";
+import React, { useEffect, useState } from "react";
 import FeedContainer from "./feed-container";
 import Refresh from "./refresh";
 import { FeedContext } from "../../lib/context";
 import Item from "../../lib/item-class";
+import populateFeed from "../../lib/populate-feed"; 
 
 export default function Main() {
-    const [feed, setFeed] = useState<Item[] | undefined>([]);
-  
-    return (
-      <main>
-        <FeedContext.Provider value={{ feed, setFeed }}>
-          <Refresh />
-          <FeedContainer feed={feed} />
-        </FeedContext.Provider>
-      </main>
-    );
-  }
+  const [feed, setFeed] = useState<Item[] | undefined>([]);
+
+  useEffect(() => {
+    const fetchInitialFeed = async () => {
+      try {
+        const initialFeedData = await populateFeed();
+        setFeed(initialFeedData);
+      } catch (error) {
+        console.error("Error fetching initial feed data:", error);
+      }
+    };
+
+    fetchInitialFeed();
+  }, []);
+
+  return (
+    <main>
+      <FeedContext.Provider value={{ feed, setFeed }}>
+        <Refresh />
+        <FeedContainer feed={feed} />
+      </FeedContext.Provider>
+    </main>
+  );
+}
